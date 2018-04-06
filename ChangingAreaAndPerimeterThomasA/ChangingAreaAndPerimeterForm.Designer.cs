1b0332a973cae36604e942bf40bca35a993f287f
Thomas823 <35612259+Thomas823@users.noreply.github.com>
2018-04-05 21:48:29 -0400

    Final

--- ChangingAreaAndPerimeterThomasA/ChangingAreaAndPerimeterForm.Designer.cs
+++ ChangingAreaAndPerimeterThomasA/ChangingAreaAndPerimeterForm.Designer.cs
@@ -28,20 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblLength = new System.Windows.Forms.Label();
+            this.lblWidth = new System.Windows.Forms.Label();
+            this.lblPerimeterAnswer = new System.Windows.Forms.Label();
+            this.lblAreaAnswer = new System.Windows.Forms.Label();
+            this.txtLengthInput = new System.Windows.Forms.TextBox();
+            this.txtWidthInput = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lblLength
+            // 
+            this.lblLength.AutoSize = true;
+            this.lblLength.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLength.Location = new System.Drawing.Point(61, 64);
+            this.lblLength.Name = "lblLength";
+            this.lblLength.Size = new System.Drawing.Size(101, 24);
+            this.lblLength.TabIndex = 0;
+            this.lblLength.Text = "Length (m)";
+            // 
+            // lblWidth
+            // 
+            this.lblWidth.AutoSize = true;
+            this.lblWidth.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWidth.Location = new System.Drawing.Point(61, 115);
+            this.lblWidth.Name = "lblWidth";
+            this.lblWidth.Size = new System.Drawing.Size(91, 24);
+            this.lblWidth.TabIndex = 1;
+            this.lblWidth.Text = "Width (m)";
+            // 
+            // lblPerimeterAnswer
+            // 
+            this.lblPerimeterAnswer.AutoSize = true;
+            this.lblPerimeterAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPerimeterAnswer.Location = new System.Drawing.Point(61, 184);
+            this.lblPerimeterAnswer.Name = "lblPerimeterAnswer";
+            this.lblPerimeterAnswer.Size = new System.Drawing.Size(0, 24);
+            this.lblPerimeterAnswer.TabIndex = 2;
+            // 
+            // lblAreaAnswer
+            // 
+            this.lblAreaAnswer.AutoSize = true;
+            this.lblAreaAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAreaAnswer.Location = new System.Drawing.Point(61, 231);
+            this.lblAreaAnswer.Name = "lblAreaAnswer";
+            this.lblAreaAnswer.Size = new System.Drawing.Size(0, 24);
+            this.lblAreaAnswer.TabIndex = 3;
+            // 
+            // txtLengthInput
+            // 
+            this.txtLengthInput.Location = new System.Drawing.Point(255, 69);
+            this.txtLengthInput.Name = "txtLengthInput";
+            this.txtLengthInput.Size = new System.Drawing.Size(100, 20);
+            this.txtLengthInput.TabIndex = 4;
+            // 
+            // txtWidthInput
+            // 
+            this.txtWidthInput.Location = new System.Drawing.Point(255, 120);
+            this.txtWidthInput.Name = "txtWidthInput";
+            this.txtWidthInput.Size = new System.Drawing.Size(100, 20);
+            this.txtWidthInput.TabIndex = 5;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(436, 85);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(114, 43);
+            this.btnCalculate.TabIndex = 6;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
             // 
             // frmChangingAreaAndPerimeter
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(572, 288);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtWidthInput);
+            this.Controls.Add(this.txtLengthInput);
+            this.Controls.Add(this.lblAreaAnswer);
+            this.Controls.Add(this.lblPerimeterAnswer);
+            this.Controls.Add(this.lblWidth);
+            this.Controls.Add(this.lblLength);
             this.Name = "frmChangingAreaAndPerimeter";
             this.Text = "Changing Area and Perimeter By Thomas Aubin";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblLength;
+        private System.Windows.Forms.Label lblWidth;
+        private System.Windows.Forms.Label lblPerimeterAnswer;
+        private System.Windows.Forms.Label lblAreaAnswer;
+        private System.Windows.Forms.TextBox txtLengthInput;
+        private System.Windows.Forms.TextBox txtWidthInput;
+        private System.Windows.Forms.Button btnCalculate;
     }
 }
 
--- ChangingAreaAndPerimeterThomasA/ChangingAreaAndPerimeterForm.cs
+++ ChangingAreaAndPerimeterThomasA/ChangingAreaAndPerimeterForm.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Created by: Thomas Aubin
+ * Created on: 5 April, 2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #9 - Changing Area And Perimeter
+ * This program allows for user input to calculate the area and perimeter of a rectangle or square
+*/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +20,27 @@ namespace ChangingAreaAndPerimeterThomasA
 {
     public partial class frmChangingAreaAndPerimeter : Form
     {
+        // Create variables
+        double length, width, area, perimeter;
+
         public frmChangingAreaAndPerimeter()
         {
             InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {   
+            // Get length and width from text boxes
+            length = double.Parse(txtLengthInput.Text);
+            width = double.Parse(txtWidthInput.Text);
+
+            // Calculate area and perimeter
+            area = length * width;
+            perimeter = (2 * length) + (2 * width);
+
+            // Update answers in labels
+            lblAreaAnswer.Text = Convert.ToString(area) + " squared meters";
+            lblPerimeterAnswer.Text = Convert.ToString(perimeter) + " meters";
         }
     }
 }
